d537f531567d871d9505bc143e101fa988a56967
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-02-17 07:16:17 +0700

    access modifier variable/function

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,6 +2,19 @@ package helper // <-- Package
 
 import "fmt"
 
-func SayHello(name string) {
+/**
+* Access modifier => How the variable || function that can accessed out of package
+* If variable/function name start from Uppercase, i.e SayHello, SayGoodBye <-- it can access from out of package
+* But, if the variable/function name start from Lowercase, i.e. sayHello, sayGoodBy, etc. <-- it can`t access out of the package
+*/
+
+var AppName = "Go Basic" // <-- Can accessed
+var version = 1.0		 // <-- Cannot accessed
+
+func SayHello(name string) { 		// <-- Can accessed
 	fmt.Println("Hello", name)
+}
+
+func sayGoodBye(name string) {		// <-- Cannot accessed
+	fmt.Println("Good bye", name)
 }
--- import.go
+++ import.go
@@ -1,8 +1,15 @@
 package main
 
 // Import from package helper
-import "go-basic/helper"
+import (
+	"fmt"
+	"go-basic/helper"
+)
 
 func main() {
 	helper.SayHello("Ahmad")
+	// helper.sayGoodBye("Hanafi") <-- It will error
+
+	fmt.Println("App name:", helper.AppName)
+	// fmt.Println("App version:", helper.version) <-- It will error
 }
