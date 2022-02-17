5313c34e5005d369987f41a2f57c9f1608a564c8
Ahmad Hanafi <ahanafi.id@gmail.com>
2022-02-17 07:06:30 +0700

    import and package

---
+++ helper/sayhello.go
@@ -0,0 +1,7 @@
+package helper // <-- Package
+
+import "fmt"
+
+func SayHello(name string) {
+	fmt.Println("Hello", name)
+}
---
+++ import.go
@@ -0,0 +1,8 @@
+package main
+
+// Import from package helper
+import "go-basic/helper"
+
+func main() {
+	helper.SayHello("Ahmad")
+}
